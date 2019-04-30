ddbc1a83e898f4ec8ef541ea539bcc7e78e761ee
andreynho2006 <andreynho2006@gmail.com>
2019-04-30 20:18:11 +0300

    set the id of the full post equal to the state selectedPostId

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -25,12 +26,18 @@ class Blog extends Component {
                 //console.log(response);
             });
     }
+
+    postselectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
         const posts = this.state.posts.map(post => {
             return <Post 
                         key={post.id} 
                         title={post.title}
-                        author = {post.author} />
+                        author = {post.author}
+                        clicked={() => this.postselectedHandler(post.id)} />
         });
 
         return (
@@ -39,7 +46,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
